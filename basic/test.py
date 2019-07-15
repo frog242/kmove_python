7be27975db7bb6ad5d7b1d964e810b2ef96c13d2
frog242 <rornfl242@gmail.com>
2019-07-15 17:45:59 +0900

    20190715 수업 최종본

--- basic/test.py
+++ basic/test.py
@@ -1,5 +1,8 @@
 print('hi')
 print("hi")
-print('''hi
-        
-        hi''')
+food="python's favorite\n \"food\" is perl"
+print(food[5:])
+a = 6.6666666
+print('i eat %0.8f apples.' %a)
+
+print("python's {1}favor{0}ite".format(12,20))
