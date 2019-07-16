b7ad28913caaeadce5da982a769234d83e83fabc
frog242 <rornfl242@gmail.com>
2019-07-16 19:30:01 +0900

    20190716 로또 과제

--- basic/lotto_test.py
+++ basic/lotto_test.py
@@ -1,6 +1,21 @@
 import random
-base = [0]
+
+
 
 for i in range(0,5):
-    for i in range
-    random.randint(1,45)
+    base =[]
+    lotto=[]
+    for j in range(0,6):
+        number = random.randint(1,45)
+        base.append(number)
+        for k in range(0,j):
+            while base[k] == number:
+                number = random.randint(1,45)
+                if base[k] != number:
+                    break
+
+        lotto.append(number)
+        
+
+    lotto.sort()   
+    print(lotto)
